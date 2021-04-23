c650aaf739f6d0fdf898a0fe535be49a4744fd3f
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-23 13:21:49 -0500

    Create audit logs controller

---
+++ Alpha.Web.API/Controllers/AuditLogsController.cs
@@ -0,0 +1,27 @@
+﻿using Alpha.Web.API.Domain.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Alpha.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditLogsController : ControllerBase
+    {
+        private readonly IAuditLogsService _auditLogService;
+
+        public AuditLogsController(IAuditLogsService auditLogService)
+        {
+            _auditLogService = auditLogService;
+        }
+
+        [HttpGet]
+        [Route("api/auditLogs")]
+        public async Task<IActionResult> GetAuditLogsByCriteria(string action, DateTime endDate,
+            DateTime startDate, string tableName, int tableId, int userId)
+        {
+            return Ok(await _auditLogService.GetByCriteriaAsync(action, endDate, startDate, tableName, tableId, userId));
+        }
+    }
+}

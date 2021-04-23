0d4e9db95217c31008bc017f144fa94015ef8178
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-23 13:14:02 -0500

    Create generic repositoy interface

---
+++ Alpha.Web.API/Data/Repositories/IGenericReadOnlyRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alpha.Web.API.Data.Repositories
+{
+    public interface IGenericReadOnlyRepository<Entity> where Entity : class
+    {
+        Task<IEnumerable<Entity>> GetAllAsync();
+        Task<Entity> GetByIdAsync(int id);
+    }
+}

8e674003b3a35a19b6bc4d9d439864a87c9201e2
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-23 14:57:06 -0500

    Create string ramdom generator helper

---
+++ Alpha.Tests/Util/UtilRamdomGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Alpha.Tests.Util
+{
+    public static class UtilRandomGenerator
+    {
+        public static string GenerateString(int length)
+        {
+            Random random = new Random();
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static string GenerateNumber(int length)
+        {
+            Random random = new Random();
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}

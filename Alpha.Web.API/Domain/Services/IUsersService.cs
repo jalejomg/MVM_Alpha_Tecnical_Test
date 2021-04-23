fca3a53b8414df5d6818e548f967fbd664fbda7f
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-23 13:18:20 -0500

    Create users service interface

---
+++ Alpha.Web.API/Domain/Services/IUsersService.cs
@@ -0,0 +1,15 @@
+﻿using Alpha.Web.API.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alpha.Web.API.Domain.Services
+{
+    public interface IUsersService
+    {
+        Task<UserModel> GetByIdAsync(int userId);
+        Task<ResponseModel<IEnumerable<UserModel>>> ListAsync();
+        Task<int> CreateAsync(UserModel userModel);
+        Task<int> UpdateAsync(int userId, UserModel userModel);
+        Task DeleteAsync(int userId);
+    }
+}

cd6ab30c5c06b95968ea733c0a265d9642f3f129
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-23 13:17:52 -0500

    Create custom exeptions

---
+++ Alpha.Web.API/CustomExceptions/BaseCustomException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace Alpha.Web.API.CustomExceptions
+{
+    public class BaseCustomException : Exception
+    {
+        public BaseCustomException(string message, string description, HttpStatusCode code) : base(message)
+        {
+            Code = code;
+            Description = description;
+        }
+
+        public HttpStatusCode Code { get; set; }
+        public string Description { get; set; }
+    }
+}
+
---
+++ Alpha.Web.API/CustomExceptions/NotFoundCustomException.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Alpha.Web.API.CustomExceptions
+{
+    public class NotFoundCustomException : BaseCustomException
+    {
+        public NotFoundCustomException(string message) : base(message, string.Empty, HttpStatusCode.NotFound)
+        {
+        }
+
+        public NotFoundCustomException(string message, string description) : base(message, description,
+            HttpStatusCode.NotFound)
+        {
+        }
+    }
+}

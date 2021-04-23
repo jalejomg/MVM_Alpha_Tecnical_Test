74c6ef0a8f528586dfa71b7fc9a9738dce566771
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-23 13:19:02 -0500

    Create messages service interface

---
+++ Alpha.Web.API/Domain/Services/IMessagesService.cs
@@ -0,0 +1,16 @@
+﻿using Alpha.Web.API.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alpha.Web.API.Domain.Services
+{
+    public interface IMessagesService
+    {
+        Task<MessageModel> GetByIdAsync(int messageId);
+        Task<ResponseModel<IEnumerable<MessageModel>>> ListAsync();
+        Task<int> CreateAsync(MessageModel messageModel);
+        Task<int> UpdateAsync(int messageId, MessageModel messageModel);
+        Task DeleteAsync(int messageId);
+        Task SendAsync(int messageId);
+    }
+}

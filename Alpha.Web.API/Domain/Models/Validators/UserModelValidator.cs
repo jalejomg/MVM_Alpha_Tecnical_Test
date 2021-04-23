ff00628ac7875a6e32fd360080bd812a772c066a
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-23 13:15:58 -0500

    Create models validators

---
+++ Alpha.Web.API/Domain/Models/Validators/UserModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace Alpha.Web.API.Domain.Models.Validators
+{
+    public class UserModelValidator : AbstractValidator<UserModel>
+    {
+        public UserModelValidator()
+        {
+            RuleFor(model => model.Name)
+                .NotEmpty().WithMessage("Name parameter can't be empty")
+                .MaximumLength(50).WithMessage("Name parameter must have less than 50 characters");
+
+            RuleFor(model => model.LastName)
+                .NotEmpty().WithMessage("LastName parameter can't be empty")
+                .MaximumLength(50).WithMessage("LastName parameter must have less than 50 characters");
+
+            RuleFor(model => model.Email)
+                .NotEmpty().WithMessage("Email parameter can't be empty")
+                .MaximumLength(100).WithMessage("Email parameter must have less than 50 characters");
+        }
+    }
+}

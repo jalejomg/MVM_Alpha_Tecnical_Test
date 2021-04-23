854250d99cf5386a9d3cbefd1321ec6731cba497
Jose Alejandro Martinez Giral <jalejomg@outlook.com>
2021-04-23 13:21:11 -0500

    Create users controller

---
+++ Alpha.Web.API/Controllers/UsersController.cs
@@ -0,0 +1,85 @@
+﻿using Alpha.Web.API.CustomExceptions;
+using Alpha.Web.API.Domain.Models;
+using Alpha.Web.API.Domain.Services;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Alpha.Web.API.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public IUsersService _userService;
+
+        public UsersController(IUsersService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("api/users/")]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _userService.ListAsync());
+        }
+
+        [HttpGet]
+        [Route("api/users/{userId:int}")]
+        public async Task<IActionResult> GetById(int userId)
+        {
+            try
+            {
+                return Ok(await _userService.GetByIdAsync(userId));
+            }
+            catch (NotFoundCustomException exception)
+            {
+                return NotFound(exception.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/users/")]
+        public async Task<IActionResult> Create(UserModel user)
+        {
+            try
+            {
+                return Ok(await _userService.CreateAsync(user));
+            }
+            catch (ValidationException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("api/users/{userId:int}")]
+        public async Task<IActionResult> Update(int userId, UserModel user)
+        {
+            try
+            {
+                return Ok(await _userService.UpdateAsync(userId, user));
+            }
+            catch (ValidationException exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/users/{userId:int}")]
+        public async Task<IActionResult> Delete(int userId)
+        {
+            try
+            {
+                await _userService.DeleteAsync(userId);
+                return Ok();
+            }
+            catch (NotFoundCustomException exception)
+            {
+                return NotFound(exception.Message);
+            }
+        }
+    }
+}
